125d8a1d3b2266915d609819d5d1f8a46c711ec5
William Ford <wford1.home@gmail.com>
2021-04-10 14:02:11 +0100

    Working App

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const weather = document.querySelector('p');
 async function getWeather() {
-    const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Stamford,uk&APPID=aba6c5f8e7cebde4bbf7faaa6af078bf", {mode: "cors"});
-    const weatherData = await response.json();
-    console.log(weatherData.weather[0].description);
-    weather = weatherData.weather[0].description;
+  const location = document.getElementById('location').value;
+  const response = await fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID=aba6c5f8e7cebde4bbf7faaa6af078bf",
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  const weatherContent = weatherData.weather[0].description;
+
+  const container = document.querySelector("#weather");
+  
+  container.textContent = "Todays weather : " + weatherContent;
+  
+
+  
 }
 
-getWeather();
 
